f2bf8181897e4ed33a8f92d6b63dfbaff6214653
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2024-09-11 17:23:07 +0200

    BAZE II

--- Baze podataka II - IV SEMESTAR/Vjezbe3/Vjezba3Revisited.sql
+++ Baze podataka II - IV SEMESTAR/Vjezbe3/Vjezba3Revisited.sql
@@ -40,3 +40,89 @@ Northwind.dbo.[Order Details]
 SELECT *
 FROM Northwind.dbo.[Order Details] as O
 WHERE O.Quantity > 100 AND O.Discount > 0
+
+SELECT * FROM
+Northwind.dbo.Customers
+
+
+/* ZADATAK 11. */
+SELECT C.CompanyName, C.Phone
+FROM Northwind.dbo.Customers as C
+WHERE C.CompanyName LIKE '%Restaurant%' 
+AND C.CompanyName NOT LIKE '%-%'
+
+/* ZADATAK 12. */
+SELECT *
+FROM Northwind.dbo.Products as P
+WHERE P.ProductName LIKE '[CG]_[AO]%'
+
+/* ZADATAK 13. */
+SELECT *
+FROM Northwind.dbo.Products as P
+WHERE (P.ProductName LIKE '[LT]%' OR P.ProductID = 46) 
+AND (P.UnitPrice BETWEEN 10 AND 50)
+
+SELECT *
+FROM Northwind.dbo.Products as P
+WHERE (P.ProductName LIKE '[LT]%' OR P.ProductID = 46) 
+AND (P.UnitPrice >= 10 AND P.UnitPrice <= 50)
+
+/* ZADATAK 14. */
+SELECT P.ProductName, P.UnitPrice, 
+(UnitsOnOrder - UnitsInStock) AS StockDifference
+FROM Northwind.dbo.Products as P
+WHERE P.UnitsInStock < P.UnitsOnOrder
+
+
+/* ZADATAK 15. */
+SELECT SupplierID, 
+    CompanyName, 
+    ContactName, 
+    Country, 
+    Phone, 
+    COALESCE(Fax, 'Nema unesen faks broj') AS FaxStatus
+FROM Northwind.dbo.Suppliers as S
+WHERE (S.Country = 'Spain' OR S.Country = 'Germany') 
+AND S.Fax IS NULL
+
+/* PUBS. */
+/* ZADATAK 16. */
+
+SELECT * FROM pubs.dbo.authors
+
+SELECT A.au_id, A.au_fname + ' ' + A.au_lname AS ImePrezime, A.city
+FROM pubs.dbo.authors as A
+WHERE (A.au_id LIKE '8%' OR A.city = 'Salt Lake City') AND 
+A.[contract] = 1
+
+/* ZADATAK 17. */
+SELECT DISTINCT T.type
+FROM pubs.dbo.titles as T
+ORDER BY 1 ASC
+
+/* ZADATAK 18. */
+SELECT S.stor_id, S.ord_num, S.qty
+FROM pubs.dbo.sales as S 
+WHERE S.qty BETWEEN 10 AND 50
+ORDER BY 3 DESC
+
+SELECT S.stor_id, S.ord_num, S.qty
+FROM pubs.dbo.sales as S 
+WHERE S.qty >= 10 AND S.qty <= 50
+ORDER BY 3 DESC
+
+/* ZADATAK 19. */
+
+SELECT T.title, T.[type], T.price, 
+    CAST(ROUND(T.price * 0.20, 2) AS DECIMAL(10, 2)) AS [20% od cijene],
+    CAST(ROUND(T.price * 0.80, 2) AS DECIMAL(10, 2)) AS [Cijena umanjena za 20%]
+FROM pubs.dbo.titles as T
+WHERE T.price IS NOT NULL
+ORDER BY 2 ASC, 3 DESC
+
+/* ZADATAK 19. */
+
+SELECT S.ord_num, S.ord_date, S.qty
+FROM pubs.dbo.sales as S
+
+/* ZADATAK 20. */
---
+++ Baze podataka II - IV SEMESTAR/Vjezbe4/Vjezbe4Ponavljanje2.sql
@@ -0,0 +1,95 @@
+﻿
+
+/*Iz tabele HumanResources.Employee baze 
+AdventureWorks2017 iz kolone LoginID izvući 
+ime uposlenika.  */
+
+/*
+LEFT and RIGHT
+Extract the first 4 characters from the string 'Database Management'.
+Extract the last 5 characters from the string 'Database Management'.
+
+CHARINDEX
+Find the position of the first occurrence of the letter 'a' in the string 'Advanced SQL Querying'.
+Find the position of the word 'SQL' in the string 'Learning SQL with Advanced Topics'.
+
+PATINDEX
+Find the position of the first digit in the string 'Data2024'.
+Find the position of the first occurrence of any uppercase letter in the string 'sqlQueries'.
+
+SUBSTRING
+Extract a substring starting from the 5th character and of length 7 from the string 'Comprehensive SQL Tutorial'.
+Extract a substring starting from the 2nd character and of length 10 from the string 'Master SQL Functions'.
+
+UPPER and LOWER
+Convert the string 'SQL Fundamentals' to uppercase.
+Convert the string 'ADVANCED SQL' to lowercase.
+
+LEN
+Determine the length of the string 'String Length Calculation'.
+Determine the length of the string 'Function Testing'.
+
+REPLACE
+Replace all occurrences of 'i' with 'X' in the string 'SQL Injection'.
+Replace 'Database' with 'DB' in the string 'Database Management System'.
+
+STUFF
+Replace 3 characters starting from the 6th position in the string 'Introduction to SQL' with 'FUN'.
+Replace 4 characters starting from the 5th position in the string 'Programming Basics' with 'ADV'.
+
+STR
+Convert the number 4567 to a string.
+Convert the number 123 to a string with a width of 5.
+
+REVERSE
+Reverse the string 'SQL Query'.
+Reverse the string 'Data Analysis'.
+
+*/
+
+SELECT E.LoginID, SUBSTRING(E.LoginID, CHARINDEX(E.LoginID, '\',1), LEN(E.LoginID))
+FROM AdventureWorks2017.HumanResources.Employee as E
+
+/*
+LEFT and RIGHT
+Extract the first 4 characters from the string 'Database Management'.
+Extract the last 5 characters from the string 'Database Management'. */
+
+SELECT LEFT('Database Management', 4)
+SELECT RIGHT('Database Management', 5)
+
+/* 
+CHARINDEX
+Find the position of the first occurrence of the letter 'a' in the string 'Advanced SQL Querying'.
+Find the position of the word 'SQL' in the string 'Learning SQL with Advanced Topics'.
+*/
+SELECT CHARINDEX('a', 'Advanced SQL Querying', 0)
+SELECT CHARINDEX('SQL', 'Learning SQL with Advanced Topics', 0)
+
+/* PATINDEX
+Find the position of the first digit in the string 'Data2024'.
+Find the position of the first occurrence of any uppercase letter in the string 'sqlQueries'. */
+
+SELECT PATINDEX('%[0-9]%', 'Data2024')
+SELECT PATINDEX('%[A-Z]%', 'sqlqueries')
+
+/* SUBSTRING
+Extract a substring starting from the 5th character and of length 7 from the string 'Comprehensive SQL Tutorial'.
+Extract a substring starting from the 2nd character and of length 10 from the string 'Master SQL Functions'. */
+
+SELECT SUBSTRING('Comprehensive SQL Tutorial', 5, 7)
+SELECT SUBSTRING('Master SQL Functions', 2, 10)
+
+/* REPLACE
+Replace all occurrences of 'i' with 'X' in the string 'SQL Injection'.
+Replace 'Database' with 'DB' in the string 'Database Management System'. */
+
+SELECT REPLACE('SQL Injection', 'i', 'X')
+SELECT REPLACE('Database Management System', 'Database', 'DB')
+
+/* STUFF
+Replace 3 characters starting from the 6th position in the string 'Introduction to SQL' with 'FUN'.
+Replace 4 characters starting from the 5th position in the string 'Programming Basics' with 'ADV'. */
+
+SELECT STUFF('Introduction to SQL', 6, 3, 'FUN')
+SELECT STUFF('Programming Basics', 5, 4, 'ADV')
